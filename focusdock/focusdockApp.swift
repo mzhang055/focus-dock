85a2c59054bca04bacc0974057d5a26f9c8521a1
mzhang055 <mzhang055@gmail.com>
2025-06-17 23:59:30 -0400

    Initial Commit

---
+++ focusdock/focusdockApp.swift
@@ -0,0 +1,17 @@
+//
+//  focusdockApp.swift
+//  focusdock
+//
+//  Created by Michelle Zhang on 2025-06-17.
+//
+
+import SwiftUI
+
+@main
+struct focusdockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ focusdockTests/focusdockTests.swift
@@ -0,0 +1,17 @@
+//
+//  focusdockTests.swift
+//  focusdockTests
+//
+//  Created by Michelle Zhang on 2025-06-17.
+//
+
+import Testing
+@testable import focusdock
+
+struct focusdockTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
